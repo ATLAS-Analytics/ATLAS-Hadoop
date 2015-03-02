82ca63126a1b4a21c8d630e5ed99ff5987724ea3
Ilija Vukotic <ivukotic@cern.ch>
2015-03-02 16:00:52 -0600

    dir change

--- pigCodes/Panda/OverflowMatrix/run.sh
+++ pigCodes/Panda/OverflowMatrix/run.sh
@@ -3,7 +3,7 @@
 # this script is called from a cron once a day after script importing the new data
 
 # it runs a pig script to extract overflow info
-cd /afs/cern.ch/user/i/ivukotic/ATLAS-data-analytics/pigCodes/Panda/OverflowMatrix/
+cd /afs/cern.ch/user/i/ivukotic/ATLAS-Hadoop/pigCodes/Panda/OverflowMatrix/
 pig OverflowMatrix.pig
 
 # then it runs a python code that takes results and uploads it to FSB (GAE)
