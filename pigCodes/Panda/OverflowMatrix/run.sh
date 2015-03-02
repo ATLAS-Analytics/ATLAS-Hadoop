99e1039493e8c125228b4e6b097aec63275e3f96
Ilija Vukotic <ivukotic@cern.ch>
2015-03-02 17:36:05 -0600

    now files in directories per day.

--- pigCodes/Panda/OverflowMatrix/run.sh
+++ pigCodes/Panda/OverflowMatrix/run.sh
@@ -3,8 +3,11 @@
 # this script is called from a cron once a day after script importing the new data
 
 # it runs a pig script to extract overflow info
+
 cd /afs/cern.ch/user/i/ivukotic/ATLAS-Hadoop/pigCodes/Panda/OverflowMatrix/
-pig OverflowMatrix.pig
+DateToProcess=$(date +%Y-%m-%d)
+echo "Processing... "${DateToProcess}
+pig -f OverflowMatrix.pig -param INPD=${DateToProcess} 
 
 # then it runs a python code that takes results and uploads it to FSB (GAE)
 python OverflowMatrixSender.py
