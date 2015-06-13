4401f2f0f429555191ecf093b2fb6d9a3b5849a6
Ilija Vukotic <ivukotic@cern.ch>
2015-06-14 00:16:07 +0200

    dbg

--- pigCodes/Panda/JobArchive/myudfs.py
+++ pigCodes/Panda/JobArchive/myudfs.py
@@ -6,10 +6,12 @@ def deriveTimes(origString):
 @outputSchema('tuple( walltime:int, cpueff:float, queue_time:int)')
 def deriveDurationAndCPUeff(CREATIONTIME,STARTTIME,ENDTIME,CPUCONSUMPTIONTIME):
     walltime=ENDTIME-STARTTIME
-    if walltime>0 and CPUCONSUMPTIONTIME!='': 
-        cpueff = int(CPUCONSUMPTIONTIME)/walltime
-    else:
-        cpueff = 0
+    cpueff=0
+    try:
+        if walltime>0 and CPUCONSUMPTIONTIME!='': 
+            cpueff = int(CPUCONSUMPTIONTIME)/walltime
+    except:
+        print "problem with cpueff: "+CPUCONSUMPTIONTIME
     queue_time=STARTTIME-CREATIONTIME
     return (walltime,cpueff,queue_time)
     
