67f84452617d7e99b594efa49a419dead8195411
Ilija Vukotic <ivukotic@cern.ch>
2015-06-13 23:39:19 +0200

    dbg

--- pigCodes/Panda/JobArchive/myudfs.py
+++ pigCodes/Panda/JobArchive/myudfs.py
@@ -1,9 +1,9 @@
-@outputSchema('tuple( timeGetJob:integer, timeStageIn:integer, timeExe:integer, timeStageOut:integer, timeSetup:integer)')
+@outputSchema('tuple( timeGetJob:int, timeStageIn:int, timeExe:int, timeStageOut:int, timeSetup:int)')
 def deriveTimes(origString):
     times=origString.split('|')
     return times
     
-@outputSchema('tuple( walltime:integer, cpueff:float, queue_time:integer)')
+@outputSchema('tuple( walltime:int, cpueff:float, queue_time:int)')
 def deriveDurationAndCPUeff(origTuple):
     walltime=origTuple[2]-origTuple[1]
     if walltime>0: cpueff = origTuple[3]/walltime
