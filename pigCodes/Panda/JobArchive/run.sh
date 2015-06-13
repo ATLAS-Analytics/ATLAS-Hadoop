2b7eed3621ace6a05ffaff28ee2801c640aa183c
Ilija Vukotic <ivukotic@cern.ch>
2015-06-13 22:20:25 +0200

    starting jobArchive import

---
+++ pigCodes/Panda/JobArchive/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# this script is called from a cron once a day after script importing the new data
+# it runs a pig script to index the new data in ES.
+
+cd /afs/cern.ch/user/i/ivukotic/ATLAS-Hadoop/pigCodes/Panda/JobArchive/
+DateToProcess=$(date +%Y-%m-%d)
+DateToProcess=2015-06-01
+echo "Processing... "${DateToProcess}
+pig -f OverflowMatrix.pig -param INPD=${DateToProcess} 
+
+
+
+
