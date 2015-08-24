df08fefa7fde856df37754746dc1e2e16f4301bc
Ilija Vukotic <ivukotic@cern.ch>
2015-08-24 11:42:27 -0500

    split moving to hadoop and ES indexing

--- import/PandaImporter.sh
+++ import/PandaImporter.sh
@@ -9,17 +9,10 @@ cd /afs/cern.ch/user/i/ivukotic/ATLAS-Hadoop/import
 
 startDate=$(date +%Y-%m-%d -d "-4day")
 endDate=$(date +%Y-%m-%d  -d "-3day")
-JobImportwp.sh ${startDate} endDate
+./JobImportwp.sh ${startDate} ${endDate}
 
 echo "DONE"
 
-echo "  ******************************* indexing Panda Jobs into ES **************************"
-
-cd /afs/cern.ch/user/i/ivukotic/ATLAS-Hadoop/pigCodes/Panda/JobArchive/
-pig -4 log4j.properties -f toESnew.pig -param INPD=${startDate}
-
-
-echo "DONE"
 
 #echo "  *******************************  importing jobs parameters table  *******************************  "
 
@@ -32,15 +25,15 @@ echo "DONE"
 
 #echo "DONE"
 
-echo "  *******************************  importing jobs status table  *******************************  "
-sqoop job --exec JOBSSTATUSimport
+#echo "  *******************************  importing jobs status table  *******************************  "
+#sqoop job --exec JOBSSTATUSimport
 
-hdfs dfs -mkdir /atlas/analytics/panda/JOBS_STATUSLOG/${DateToMoveTo:0:7}/${DateToMoveTo}
-hdfs dfs -mv '/atlas/analytics/panda/JOBS_STATUSLOG/part*' /atlas/analytics/panda/JOBS_STATUSLOG/${DateToMoveTo:0:7}/${DateToMoveTo}
+#hdfs dfs -mkdir /atlas/analytics/panda/JOBS_STATUSLOG/${DateToMoveTo:0:7}/${DateToMoveTo}
+#hdfs dfs -mv '/atlas/analytics/panda/JOBS_STATUSLOG/part*' /atlas/analytics/panda/JOBS_STATUSLOG/${DateToMoveTo:0:7}/${DateToMoveTo}
 
 #sqoop job -D sqoop.metastore.client.record.password=true --create JOBSSTATUSimport  -- import --connect "jdbc:oracle:thin:@//db-d0002.cern.ch:10654/int8r.cern.ch" --table ATLAS_PANDA.JOBS_STATUSLOG --username DMUSER --P --split-by PANDAID --as-avrodatafile --target-dir /atlas/analytics/panda/JOBS_STATUSLOG --columns PANDAID,MODIFICATIONTIME,JOBSTATUS,PRODSOURCELABEL,CLOUD,COMPUTINGSITE --check-column MODIFICATIONTIME --incremental append --last-value '01-Jan-10'
 
-echo "DONE"
+#echo "DONE"
 
 # echo "  *******************************  importing panda log table  *******************************  "
 # sdate=$(date --date="2 days ago" +%d-%b-%Y)
