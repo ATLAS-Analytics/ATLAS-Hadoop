5096397349bd4aaf0a8005f3972150239c8ebbc1
Ilija Vukotic <ivukotic@cern.ch>
2015-09-07 16:59:50 -0500

    making ES indexer all the same.

--- pigCodes/Panda/JobArchive/myudfs.py
+++ pigCodes/Panda/JobArchive/myudfs.py
@@ -12,23 +12,24 @@ def strToTS(d):
 def deriveTimes(origString):
     if origString is None:
         return (0,0,0,0,0)
-    times=origString.split('|')
+    times = origString.split('|')
     if len(times)==4: times.append(0)
     return (int(times[0]),int(times[1]),int(times[2]),int(times[3]),int(times[4]))
+
     
 
 @outputSchema('tuple( walltime:int, cpueff:float, queue_time:int)')
 def deriveDurationAndCPUeff(CREATIONTIME,STARTTIME,ENDTIME,CPUCONSUMPTIONTIME):
     if CREATIONTIME is None or STARTTIME is None or ENDTIME is None or CPUCONSUMPTIONTIME is None:
         return (0,0.0,0)
-    CREATIONTIME = CREATIONTIME / 1000
-    STARTTIME = STARTTIME / 1000
-    ENDTIME = ENDTIME / 1000
+    CREATIONTIME   = CREATIONTIME / 1000
+    STARTTIME      = STARTTIME / 1000
+    ENDTIME        = ENDTIME / 1000
     
-    walltime = ENDTIME-STARTTIME
-    queue_time  =STARTTIME-CREATIONTIME
+    walltime       = ENDTIME - STARTTIME
+    queue_time     = STARTTIME - CREATIONTIME
     
-    cpueff=0
+    cpueff         = 0
     try:
         if walltime>0 and CPUCONSUMPTIONTIME!='':
             cpueff = float(CPUCONSUMPTIONTIME)/walltime
@@ -36,21 +37,22 @@ def deriveDurationAndCPUeff(CREATIONTIME,STARTTIME,ENDTIME,CPUCONSUMPTIONTIME):
         print "problem with cpueff: "+CPUCONSUMPTIONTIME
         
     return (walltime,cpueff,queue_time)
+
         
 @outputSchema('tuple( walltime:int, cpueff:float, queue_time:int)')
 def deriveDurationAndCPUeffNEW(CREATIONTIME,STARTTIME,ENDTIME,CPUCONSUMPTIONTIME):
     if CREATIONTIME is None or STARTTIME is None or ENDTIME is None or CPUCONSUMPTIONTIME is None:
         return (0,0.0,0)
-    CREATIONTIME = strToTS(CREATIONTIME)
-    STARTTIME = strToTS(STARTTIME)
-    ENDTIME = strToTS(ENDTIME)
+    CREATIONTIME   = strToTS(CREATIONTIME)
+    STARTTIME      = strToTS(STARTTIME)
+    ENDTIME        = strToTS(ENDTIME)
     
-    wt = ENDTIME-STARTTIME 
-    qt = STARTTIME-CREATIONTIME
-    walltime = wt.seconds + wt.days * 86400
-    queue_time  = qt.seconds + qt.days * 86400
+    wt             = ENDTIME-STARTTIME 
+    qt             = STARTTIME-CREATIONTIME
+    walltime       = wt.seconds + wt.days * 86400
+    queue_time     = qt.seconds + qt.days * 86400
     
-    cpueff=0
+    cpueff         = 0
     try:
         if walltime>0 and CPUCONSUMPTIONTIME!='':
             cpueff = float(CPUCONSUMPTIONTIME)/walltime
@@ -58,6 +60,7 @@ def deriveDurationAndCPUeffNEW(CREATIONTIME,STARTTIME,ENDTIME,CPUCONSUMPTIONTIME
         print "problem with cpueff: "+CPUCONSUMPTIONTIME
         
     return (walltime,cpueff,queue_time)
+
     
 @outputSchema('TIMESTAMP:chararray')
 def Tstamp(ts):
