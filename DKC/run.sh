ab7ea28fbba8ba81a56bffe992ad5f27c2dd885f
Ilija Vukotic <ivukotic@cern.ch>
2015-12-14 08:46:00 -0600

    For importing of the Data Knowledge Catalog

--- DKC/run.sh
+++ DKC/run.sh
@@ -3,9 +3,9 @@
 kinit ivukotic@CERN.CH -k -t /afs/cern.ch/user/i/ivukotic/ivukotic.keytab
 
 cd /afs/cern.ch/user/i/ivukotic/ATLAS-Hadoop/DKC
-echo "Indexing... CL "${DateToProcess}
+echo "Indexing... DKC "
 pig -4 log4j.properties -f toEScl.pig 
-
+echo "Done."
 
 
 
