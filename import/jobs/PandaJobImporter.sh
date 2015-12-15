013bdda939e08a9910afd444c6cb088f1f260b47
Ilija Vukotic <ivukotic@cern.ch>
2015-12-15 17:29:25 -0600

    fix.

--- import/jobs/PandaJobImporter.sh
+++ import/jobs/PandaJobImporter.sh
@@ -3,6 +3,8 @@
 # this script is called from a cron every hour
 # it scoops panda job archive info and then indexes it in ES at clemson.
 
+kinit ivukotic@CERN.CH -k -t /afs/cern.ch/user/i/ivukotic/ivukotic.keytab
+
 echo "  *******************************  importing jobs table  *******************************"
 
 cd /afs/cern.ch/user/i/ivukotic/ATLAS-Hadoop/import/jobs/
@@ -10,10 +12,13 @@ cd /afs/cern.ch/user/i/ivukotic/ATLAS-Hadoop/import/jobs/
 startDate=$(date -u '+%Y-%m-%d %H:00:00' -d "-2hour")
 endDate=$(date -u '+%Y-%m-%d %H:00:00' -d "-1hour")
 fileName=$(date -u '+%Y-%m-%d_%H' -d "-2hour")
+ind=$(date -u '+%Y-%m-%d' -d "-2hour")
+echo "start date: ${startDate}"  
+echo "end date: ${endDate}"  
+echo "file name: ${fileName}"  
+echo "index : ${ind}"  
 ./JobSqoopWP.sh "${startDate}" "${endDate}" "${fileName}"
 echo "Sqooping DONE."
 
-ind=$(date -u '+%Y-%m-%d' -d "-2hour")
 pig -4 log4j.properties -f toEScl.pig -param INPD=${fileName} -param ININD=${ind}
-
 echo "Indexing DONE."
